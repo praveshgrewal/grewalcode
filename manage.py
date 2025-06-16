00c4e51c4aaaa2a453ec814456a3f070df9b6185
PRAVESH GREWAL <118418011+praveshgrewal@users.noreply.github.com>
2025-06-16 11:42:29 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,21 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("✅ Superuser created.")
+else:
+    print("ℹ️ Superuser already exists or environment variables not set.")
 
 
 def main():
